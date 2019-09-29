7427d6e4392a24b669250192d8bb87d3ba56ed1e
笨小孩 <m15572380036@163.com>
2019-09-29 19:55:43 +0800

    修改最新版本

--- mp/src/pages/index/index.vue
+++ mp/src/pages/index/index.vue
@@ -6,33 +6,13 @@
           <div>照片24</div>
           <div class="takephoto">拍照</div>
       </div>
-<<<<<<< HEAD
-=======
-<<<<<<< 1414ebd54fedfa0f138ae9cede70d832b1bab067
->>>>>>> parent of a2f6339... 1.1.1
+
+
       <div class="content">
         <div class="nav">
           <div>按时间</div>
           <div>按人气</div>
           <div>按距离</div>
-<<<<<<< HEAD
-=======
-=======
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
->>>>>>> he
->>>>>>> parent of a2f6339... 1.1.1
         </div>
         <Theme></Theme>
         <Theme></Theme>
@@ -41,19 +21,11 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 
 import Swiper from '../../components/swiper.vue'
 import Theme from '../../components/theme.vue'
-=======
-<<<<<<< 1414ebd54fedfa0f138ae9cede70d832b1bab067
 
-import Swiper from '../../components/swiper.vue'
-import Theme from '../../components/theme.vue'
-=======
-import card from '@/components/card'
->>>>>>> he
->>>>>>> parent of a2f6339... 1.1.1
+
 export default {
   data () {
     return {
--- mp/src/pages/logs/index.vue
+++ mp/src/pages/logs/index.vue
@@ -1,8 +1,5 @@
 <template>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> parent of a2f6339... 1.1.1
+
   <div class="user">
     <img class="bg" src="/static/images/timg.jpg" background-size="cover" />
     <!-- 获取用户头像 -->
@@ -51,39 +48,12 @@
         </div>
       </div>
     </div>
-<<<<<<< HEAD
-=======
-=======
-  <div>
-      <swiper v-if="imgUrls.length > 0" indidator-dots="imgUrls.length > 1" >
-      <block v-for="(item, index) in imgUrls" :key="index" >
-        <swiper-item>
-          <image :src="item" mode="scaleToFill"></image>
-        </swiper-item>
-      </block>
-    </swiper>
 
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-      </li>
-    </ul>
->>>>>>> 1246c008f1af074febc61a2ea3d24f79f40cfe21
->>>>>>> parent of a2f6339... 1.1.1
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 const { http } = require("../../utils/ajax");
-export default {
-  data() {
-=======
-<<<<<<< HEAD
-const { http } = require("../../utils/ajax");
-export default {
-  data() {
-=======
-import { formatTime } from '@/utils/index'
 
 
 export default {
@@ -92,8 +62,6 @@ export default {
   },
 
   data () {
->>>>>>> 1246c008f1af074febc61a2ea3d24f79f40cfe21
->>>>>>> parent of a2f6339... 1.1.1
     return {
       userInfo: {}
     };
