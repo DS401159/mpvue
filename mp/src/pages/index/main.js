a09aa470ccb8929d761aeeeb0700893393f66526
ds <905477391@qq.com>
2019-09-29 19:34:54 +0800

    1.1.2

--- mp/src/pages/index/main.js
+++ mp/src/pages/index/main.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
 import App from './index'
+<<<<<<< HEAD
+=======
+<<<<<<< 1414ebd54fedfa0f138ae9cede70d832b1bab067
+=======
+
+
+>>>>>>> he
+>>>>>>> parent of a2f6339... 1.1.1
 const app = new Vue(App)
 app.$mount()
